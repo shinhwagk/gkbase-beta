8caaad44db6a3f746972a388f2eeded922de0c28
gk <shanghai_sl@hotmail.com>
2016-03-24 16:25:12 +0800

    添加document显示按钮

--- app/models/gnote/dao/entity/Content.scala
+++ app/models/gnote/dao/entity/Content.scala
@@ -6,10 +6,10 @@ import slick.driver.MySQLDriver.api._
 /**
   * Created by zhangxu on 16/3/14.
   */
-case class Content(id: Int, content_1: String, content_2: String, category_id: Int, createdata: Date, updatedata: Date)
+case class Content(id: Int, content_1: String, content_2: String, category_id: Int, document_id: Option[Int], createdata: Date, updatedata: Date)
 
 class Contents(tag: Tag) extends Table[Content](tag, "content") {
-  def * = (id, content_1, content_2, category_id, createdata, updatedata) <>(Content.tupled, Content.unapply)
+  def * = (id, content_1, content_2, category_id, document_id.?, createdata, updatedata) <>(Content.tupled, Content.unapply)
 
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
 
@@ -18,6 +18,8 @@ class Contents(tag: Tag) extends Table[Content](tag, "content") {
   def content_2 = column[String]("content_2")
 
   def category_id = column[Int]("category_id")
+
+  def document_id = column[Int]("document_id")
 
   def createdata = column[Date]("createdata")
 

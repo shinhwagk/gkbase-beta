35b7608916130126bf3323eaf85893cb001a6f52
shinhwagk <shanghai_sl@hotmail.com>
2016-06-20 18:17:01 +0800

    f

--- tableStmt/init.sql
+++ tableStmt/init.sql
@@ -1,2 +1,6 @@
 set global autocommit = 0;
 set global tx_isolation = "read-committed";
+set global character_set_database='utf8';
+set global character_set_server='utf8';
+create database g_note;
+

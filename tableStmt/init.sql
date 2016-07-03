e1841cb880bbb68d0190228fd209c4f8637d0b97
gk <shanghai_sl@hotmail.com>
2016-07-03 10:41:03 +0800

    Update init.sql

--- tableStmt/init.sql
+++ tableStmt/init.sql
@@ -2,5 +2,6 @@ set global autocommit = 0;
 set global tx_isolation = "read-committed";
 set global character_set_database='utf8';
 set global character_set_server='utf8';
+grant all PRIVILEGES on *.* to root@'%' identified by '123456aA+';
 #create database g_note;
 

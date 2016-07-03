49b2513b3bee10e42acff88492c88f7417d7f148
gk <shanghai_sl@hotmail.com>
2016-07-03 10:42:23 +0800

    Update init.sql

--- tableStmt/init.sql
+++ tableStmt/init.sql
@@ -1,4 +1,4 @@
-set global autocommit = 0;
+#set global autocommit = 0; #mysql 5.1.73 不能再global级别设置这个参数
 set global tx_isolation = "read-committed";
 set global character_set_database='utf8';
 set global character_set_server='utf8';

d534c496acfc62dbf4398fb56276e3da4207652f
gk <shanghai_sl@hotmail.com>
2016-07-06 18:31:42 +0800

    test

--- note-app/app/models/gnote/dao/Test.scala
+++ note-app/app/models/gnote/dao/Test.scala
@@ -7,14 +7,17 @@ import play.api.libs.json._
   * Created by zhangxu on 2016/7/6.
   */
 object Test {
-    val path = play.Configuration.root().getString("note.doc.content")
+      val path = play.Configuration.root().getString("note.doc.content")
 //  val path = "https://api.github.com/repos/shinhwagk/gkbase-beta/contents/document";
 
   def getDoumentSize(docId: Int): Int = {
     val github_doc_contents_url = s"$path/${docId}/README.md"
-    println(github_doc_contents_url)
-    val jsonString = Source.fromURL(github_doc_contents_url, "UTF-8").mkString;
-    (Json.parse(jsonString) \ "size").validate[Int].asOpt.get
+    try {
+      val jsonString = Source.fromURL(github_doc_contents_url, "UTF-8").mkString;
+      (Json.parse(jsonString) \ "size").validate[Int].asOpt.get
+    } catch {
+      case ex: Exception => 0
+    }
   }
 
   def judgeDoc(docId: Int): Boolean = {

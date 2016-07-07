16652b35050977ef9e402ff242b56b0bb5cae90b
gk <shanghai_sl@hotmail.com>
2016-07-07 09:40:21 +0800

    test

--- note-app/app/controllers/gNote.scala
+++ note-app/app/controllers/gNote.scala
@@ -47,7 +47,7 @@ class gNote @Inject()(implicit ec: ExecutionContext) extends Controller {
     for {
       i <- ViewDao.getDir(id)
       j <- ViewDao.deleteDir(id)
-    } yield Ok(i.head.father_id.getOrElse(0))
+    } yield Ok(0)
   }
 
   def get_content(id: Int) = Action {

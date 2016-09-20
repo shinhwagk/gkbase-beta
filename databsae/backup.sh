99cfbe28e38e9361c1d1a4e59e5422de1ceba3c5
shinhwagk <shanghai_sl@hotmail.com>
2016-09-20 13:55:17 +0800

    迁移保存

--- databsae/backup.sh
+++ databsae/backup.sh
@@ -1,1 +1,1 @@
-mysqldump -uroot -p'123456aA+' --skip-extended-insert -B g_note > backup.$(date +%Y%m%d%H%M).sql
+mysqldump -uroot -p'123456aA+' -h122.225.54.25 --skip-extended-insert -B g_note > backup.$(date +%Y%m%d%H%M).sql
